f1a2655117a985fec0cedca7c6fcdb4d7e7b3009
dxhoangsteve <sterbe2k4@gmail.com>
2025-07-22 03:48:44 +0700

    add: query check data on sqlsever

---
+++ query_sqlcmd.sql
@@ -0,0 +1,26 @@
+
+
+-- ALTER DATABASE [BioWeb] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+-- GO
+
+-- -- Drop database
+-- DROP DATABASE [BioWeb];
+-- GO
+
+select *
+from BioWeb.dbo.AdminUsers
+
+select *
+from BioWeb.dbo.SiteConfigurations
+
+select *
+from BioWeb.dbo.Articles
+
+select *
+from BioWeb.dbo.Categories
+
+select *
+from BioWeb.dbo.Contacts
+
+select *
+from BioWeb.dbo.Projects
